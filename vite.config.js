5b3912559ad1051f7d51d310723af02daee50b03
faelpatrick <faelpatrick@gmail.com>
2026-06-15 12:48:23 +0100

    Atualizar configuração do Vite para usar a base correta da URL de produção

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/',
+  base: 'https://barber.faelpatrick.com/',
   plugins: [vue()],
+  // url vue vite em producao na hostinger é: https://barber.faelpatrick.com/
+  
+  
 })
